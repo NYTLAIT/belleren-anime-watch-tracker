4e71313a694e697209805b122c1e877b50270872
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-10-16 02:49:32 -0700

    looking through json and picking out things into a object for suggestions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
@@ -8,8 +9,11 @@ print(API_URL)
 
 
 ################################ NOTE ##################################
-# return redirect(url_for('index')) #reloads the page
-# 
+#------ return redirect(url_for('index')) #reloads the page
+#------ return jsonify(queryJsom) #shows how data looks in a browser
+
+#------ Using .get() instead of ["key"] in case it doesnt exist so it wont crash
+# ex use/syntax on deeper by chatgpt: anime.get("images", {}).get("jpg", {}).get("image_url")
 ################################ NOTE ##################################
 
 #later:  Rate Limit (Jikan API has no authentication but but has rate limit of 3 requests per second) ################################## Highly doubt to go over that but you never know
@@ -27,8 +31,28 @@ def home():
     print ('------Search query:', query)
 
     if query:
-        queryOutput = requests.get(API_URL, params={'q': query}).json()
-        print('------Query output:', queryOutput)
+        queryJson = requests.get(API_URL, params={'q': query}).json()
+
+        # print('------Query Json:', queryJson)
+        #return jsonify(queryJson)
+
+        for anime in queryJson('data', []):
+            anime_info = {
+                'title': anime.get("title"),
+                'image': anime.get("images", {}).get("jpg", {}).get("image_url"), 
+                    #is jpg but there is webp
+                'episodes': anime.get("episodes"),
+                'duration': anime.get("duration")
+                # 'trailer': anime.get(),
+                'MALurl': anime.get(),
+                'status': anime.get("status"),
+                'year': anime.get("year")
+                'genres': anime.get("genres", [])
+                'demographics': anime.get 
+            }
+        
+        print(anime_info)
+
 
     
 
